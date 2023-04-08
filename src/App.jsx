ce7de80f4bc63e5e7642a950e910c54d68d9b4d4
Skyler Rasmussen <skylerrasmussen@Skylers-Air.lan>
2023-04-07 15:16:01 -1000

    Add Components file structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import styles from './styles';
 
-const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+import { Navbar, Billing, CardDeal, Business, Clients, CTA, Stats, Footer, Testimonials, Hero } from './components'
+
+const App = () => (
+    <div className="bg-primary w-full overflow-hidden">
+      <div className={`${styles.paddingx} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          navbar
+        </div>
+      </div>
+
+      {/* Hero Section */}
+      <div className={` bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          Hero
+        </div>
+      </div>
+
+      <div className={` bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          Stats
+          Busines 
+          Billing 
+          CardDeal
+          Testimonials 
+          Clients 
+          CTA 
+          Footer
+        </div>
+      </div>
+
+    </div>
+  );
 
 export default App
