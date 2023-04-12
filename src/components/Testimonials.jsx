2ced3fb29ecb962df27f9b201a784f5225a56bd8
Skyler Rasmussen <skyrasmussen13@gmail.com>
2023-04-12 13:38:57 -1000

    finish the CTA section

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -9,7 +9,7 @@ const Testimonials = () => (
         <div className="w-full flex justify-between md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]">
             <h1 className={styles.heading2}>What people are <br className="sm:block hidden"/> saying about us</h1>
             <div className="w-full md:mt-0 mt-6">
-                <p className={`${styles.paragraph} text-left max-w-[450px]`}>
+                <p className={`${styles.paragraph} text-left max-w-[450px] mt-10`}>
                     Everything you need to accept card payments and grow your business anywhere on the planet.
                 </p>
             </div>
