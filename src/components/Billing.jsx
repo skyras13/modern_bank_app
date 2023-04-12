cfabc7c358a7aa5ad9ffdadb1dff8afa408a2853
Skyler Rasmussen <skyrasmussen13@gmail.com>
2023-04-11 19:48:33 -1000

    finish the CardDeal Section

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -2,7 +2,7 @@ import { apple, bill, google } from '../assets'
 import styles, { layout } from '../styles'
 
 const Billing = () => (
-    <section id='produdct' className={layout.sectionReverse}>
+    <section id='product' className={layout.sectionReverse}>
         <div className={layout.sectionImgReverse}>
             <img src={bill} alt="billing" className='w-[100%] h-[100%] relative z-[5]'/>
 
